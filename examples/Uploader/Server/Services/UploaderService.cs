81375c183314c4ce48ce2962ed8d653f47aa184c
Michal Paukert <paukert.michal@gmail.com>
2022-11-22 15:20:00 +0100

    Memmory grow

--- examples/Uploader/Server/Services/UploaderService.cs
+++ examples/Uploader/Server/Services/UploaderService.cs
@@ -34,25 +34,11 @@ namespace Server
 
         public override async Task<UploadFileResponse> UploadFile(IAsyncStreamReader<UploadFileRequest> requestStream, ServerCallContext context)
         {
-            var uploadId = Path.GetRandomFileName();
-            var uploadPath = Path.Combine(_config["StoredFilesPath"]!, uploadId);
-            Directory.CreateDirectory(uploadPath);
-
-            await using var writeStream = File.Create(Path.Combine(uploadPath, "data.bin"));
-
             await foreach (var message in requestStream.ReadAllAsync())
             {
-                if (message.Metadata != null)
-                {
-                    await File.WriteAllTextAsync(Path.Combine(uploadPath, "metadata.json"), message.Metadata.ToString());
-                }
-                if (message.Data != null)
-                {
-                    await writeStream.WriteAsync(message.Data.Memory);
-                }
             }
 
-            return new UploadFileResponse { Id = uploadId };
+            return new UploadFileResponse { Id = "Test" };
         }
     }
 }
